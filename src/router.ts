a8957041a1c5934654c6b13e484f2216e5f65713
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-25 23:14:42 +0000

    Create basic proxy

---
+++ src/router.ts
@@ -0,0 +1,60 @@
+import * as express from 'express';
+import * as proxy from 'express-http-proxy';
+import * as vhost from 'vhost';
+import * as logger from './logger';
+import { isPortTaken, PortError } from './utils';
+
+interface Routes {
+  [i: string]: string;
+}
+
+const routes: Routes = {};
+
+let expressRouter: express.Router = express.Router();
+
+const app = express();
+
+app.use((req, res, next) => {
+  return expressRouter(req, res, next);
+});
+
+const applyRoutes = () => {
+  for (const subDomain in routes) {
+    if (routes.hasOwnProperty(subDomain)) {
+      const url = routes[subDomain];
+
+      expressRouter.use(vhost(url, proxy(subDomain)));
+    }
+  }
+};
+
+const addRoute = (subDomain: string, url: string) => {
+  routes[subDomain] = url;
+  expressRouter = express.Router();
+  applyRoutes();
+};
+
+const removeRoute = (subDomain: string, url: string) => {
+  delete routes[subDomain];
+  expressRouter = express.Router();
+  applyRoutes();
+};
+
+const router = () => {
+  const { PORT } = process.env;
+
+  const port = PORT ? parseInt(PORT, 10) : 8080;
+
+  isPortTaken(port, (error: PortError | undefined, inUse?: boolean) => {
+    if (error) {
+      logger.log(error.message);
+    } else if (inUse) {
+      logger.log(`Port ${port} is already in use`);
+    } else {
+      logger.log(`Starting router on port ${port}`);
+      app.listen(port);
+    }
+  });
+};
+
+export default router;
