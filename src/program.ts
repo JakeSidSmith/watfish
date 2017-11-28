c96d0ed275e584435cd549ac36ca996c223cd46e
Jake 'Sid' Smith <jake@dabapps.com>
2017-11-28 00:01:59 +0000

    Default env on run

--- src/program.ts
+++ src/program.ts
@@ -1,12 +1,13 @@
 import { Tree } from 'jargs';
+import { DEFAULT_ENV } from './constants';
 import * as logger from './logger';
 import { runCommand } from './run';
 const json = require('../package.json'); // tslint:disable-line:no-var-requires
 
 const version = (tree: Tree) => {
-  const { command = [] } = tree.args;
+  const { command } = tree.args;
   let { env } = tree.kwargs;
-  env = typeof env === 'string' ? env : undefined;
+  env = typeof env === 'string' ? env : DEFAULT_ENV;
 
   if (tree.flags.version) {
     logger.log(json.version);
