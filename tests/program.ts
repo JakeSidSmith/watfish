5a570c11ca3de329e6756a921bf4784e4af802d6
Jake 'Sid' Smith <jake@dabapps.com>
2017-12-11 00:24:10 +0000

    100% coverage

--- tests/program.ts
+++ tests/program.ts
@@ -1,5 +1,7 @@
+import { DEFAULT_ENV } from '../src/constants';
 import * as logger from '../src/logger';
-import version from '../src/program';
+import program from '../src/program';
+import * as run from '../src/run';
 
 // tslint:disable-next-line:no-var-requires
 const json = require('../package.json');
@@ -7,10 +9,11 @@ const json = require('../package.json');
 describe('program.ts', () => {
   beforeEach(() => {
     spyOn(logger, 'log');
+    spyOn(run, 'runCommand');
   });
 
   it('should log the version if that flag is supplied', () => {
-    version({
+    program({
       name: 'watfish',
       args: {},
       kwargs: {},
@@ -19,7 +22,7 @@ describe('program.ts', () => {
 
     expect(logger.log).not.toHaveBeenCalled();
 
-    version({
+    program({
       name: 'watfish',
       args: {},
       kwargs: {},
@@ -29,5 +32,52 @@ describe('program.ts', () => {
     });
 
     expect(logger.log).toHaveBeenCalledWith(json.version);
+  });
+
+  it('should log the version if the flag is supplied, even if there is a command', () => {
+    program({
+      name: 'watfish',
+      args: {
+        command: 'npm',
+      },
+      kwargs: {},
+      flags: {
+        version: true,
+      },
+      rest: ['install'],
+    });
+
+    expect(logger.log).toHaveBeenCalledWith(json.version);
+    expect(run.runCommand).not.toHaveBeenCalled();
+  });
+
+  it('should run a command if supplied', () => {
+    program({
+      name: 'watfish',
+      args: {
+        command: 'npm',
+      },
+      kwargs: {},
+      flags: {},
+      rest: ['install'],
+    });
+
+    expect(run.runCommand).toHaveBeenCalledWith('npm', DEFAULT_ENV, ['install']);
+  });
+
+  it('should run a command in the supplied environment', () => {
+    program({
+      name: 'watfish',
+      args: {
+        command: 'npm',
+      },
+      kwargs: {
+        env: 'custom',
+      },
+      flags: {},
+      rest: ['install'],
+    });
+
+    expect(run.runCommand).toHaveBeenCalledWith('npm', 'custom', ['install']);
   });
 });
