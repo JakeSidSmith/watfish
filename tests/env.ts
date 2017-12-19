4ab6fd0b88aec059e8886f46df859208458fd1ce
Jake 'Sid' Smith <jake@dabapps.com>
2017-12-19 16:27:19 +0000

    Write names of tests

---
+++ tests/env.ts
@@ -0,0 +1,25 @@
+import env from '../src/env';
+
+describe('env.ts', () => {
+
+  it('should log if there is no existing config when setting', () => {
+
+  });
+
+  it('should exit if there is no existing config get get, del, and log all', () => {
+
+  });
+
+  it('should log if there is no config for this project', () => {
+
+  });
+
+  it('should log if there is no environment for this project', () => {
+
+  });
+
+  it('should log the project env config', () => {
+
+  });
+
+});
